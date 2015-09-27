6f89d6de9ab6130734314496451e50d8cf9cdd9e
Javier Hernández Gil <javibilboweb@gmail.com>
2015-09-27 13:44:40 +0200

    Create bundle&extension classes for service provide

---
+++ UtilsBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the utils-bundle package.
+ *
+ * (c) Javi H. Gil <https://github.com/javihgil>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jhg\UtilsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class UtilsBundle
+ *
+ * @package Jhg\UtilsBundle
+ */
+class UtilsBundle extends Bundle
+{
+
+}
